8da232879d451a2a7a85fe8589b86170d1bd53b7
Hristick <54640308+Hristick@users.noreply.github.com>
2020-01-30 23:59:59 +0300

    Create app-form.js

---
+++ js/app-form.js
@@ -0,0 +1,6 @@
+var credit_link = document.querySelector(".credit-show");
+var credit = document.querySelector(".service-credit");
+link.addEventListener("clik", function (evt) {
+    evt.preventDefault();
+    credit.style.display="block";
+});
